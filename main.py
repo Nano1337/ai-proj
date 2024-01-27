e8ad1fd9ee4432265c46e4ae7570d4450fc90754
richardwsong <richardwsong@gmail.com>
2024-01-27 13:56:19 -0600

    edge map

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-
+import pandas as pd
+import numpy as np
 
 def main(): 
+    locs_df = pd.read_csv("road_network_locs.csv")
+    edges_df = pd.read_csv("road_network_edges.csv")
+
+    locA = edges_df["locationA"]
+    locB = edges_df["locationB"]
+
+    # make a dictionary mapping out the edges as sets to their distances 
+    edge_map = dict() 
+    for i in range(len(locA)): 
+        A = locA[i]
+        B = locB[i]
+        dist_A_B = edges_df["actualDistance"][i]
+        path = frozenset([A, B])
+        edge_map[path] = dist_A_B
+
+    print(edge_map) 
     pass
 
 if __name__ == "__main__": 
