639ba0244a43da79ae154a3885c3ccbd914302af
richardwsong <richardwsong@gmail.com>
2024-02-21 17:21:37 -0600

    dt score

--- proj2/main.py
+++ proj2/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math 
 
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
@@ -41,8 +42,25 @@ class DecisionTree:
         return node
 
     def _best_split(self, X, y):
-        pass 
-        
+        # Get threshold for each feature, equivalent to the mean of the feature 
+        thresholds = [np.mean(X[:, feature]) for feature in range(X.shape[1])]
+
+        # Calculate score for each feature 
+        score = []
+        for t in range(len(thresholds)):
+            thresh = thresholds[t] 
+            p1 = sum(X[:, t] < thresh) / X.shape[0]
+            p2 = sum(X[:, t] >= thresh) / X.shape[0]
+            p1_inner = 0 
+            p2_inner = 0
+            for i in range(1, 6):
+                p1_inner += sum(y[X[:, t] < thresh] == i) / sum(X[:, t] < thresh) * math.log(sum(y[X[:, t] < thresh] == i) / sum(X[:, t] < thresh))
+                p2_inner += sum(y[X[:, t] >= thresh] == i) / sum(X[:, t] >= thresh) * math.log(sum(y[X[:, t] >= thresh] == i) / sum(X[:, t] >= thresh))
+            p1 = p1 * p1_inner
+            p2 = p2 * p2_inner
+            score.append(p1 + p2) 
+        return np.argmin(score), thresholds[np.argmin(score)]
+            
 
     def _traverse_tree(self, x, node):
         if node.is_leaf_node():
