917e853338f91a663d0a4f1f298b05c3f3b6b8e9
Shreya Reddy <shreya.reddy@vanderbilt.edu>
2024-02-21 17:04:48 -0600

    preprocessing

--- proj2/main.py
+++ proj2/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Preprocess data
 def utility_to_label(utility):
+    utility = float(utility)
     if utility <= 0.2:
         return 'very low'
     elif utility <= 0.4:
@@ -12,6 +13,8 @@ def utility_to_label(utility):
         return 'good'
     else:
         return 'great'
-df = pd.read_csv('data.txt', sep='\t', encoding='utf-16', names=["utility", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9"])
+
+df = pd.read_csv('data.txt', sep='\t', encoding='utf-16')
 df['utility'] = df['utility'].apply(utility_to_label)
+df.to_csv('data.txt', sep='\t', encoding='utf-16', index=False)
 
