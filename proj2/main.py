0c8c973c59a0471bc18263a31825e1a44c326db3
richardwsong <richardwsong@gmail.com>
2024-02-21 17:12:48 -0600

    decision tree class

--- proj2/main.py
+++ proj2/main.py
@@ -1,5 +1,56 @@
 import pandas as pd
+import numpy as np
 
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+        self.root = None
+
+    def fit(self, X, y):
+        self.root = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return [self._traverse_tree(x, self.root) for x in X]
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [sum(y == i) for i in range(1, 6)]
+        predicted_class = num_samples_per_class.index(max(num_samples_per_class)) + 1
+        node = Node(value=predicted_class)
+
+        if depth < self.max_depth:
+            idx, thr = self._best_split(X, y)
+            if idx is not None:
+                indices_left = X[:, idx] < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature = idx
+                node.threshold = thr
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _best_split(self, X, y):
+        pass 
+        
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+        if x[node.feature] < node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+    
 # Preprocess data
 def utility_to_label(utility):
     utility = float(utility)
