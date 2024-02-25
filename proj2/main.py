8ec55649a3309477472ebcc66d8fbe35ba5a4c36
Shreya Reddy <shreya.reddy@vanderbilt.edu>
2024-02-25 17:26:54 -0600

    no sklearn

--- proj2/main.py
+++ proj2/main.py
@@ -80,21 +80,21 @@ class DecisionTree:
         return self._traverse_tree(x, node.right)
 #%% 
     
-# Preprocess data
-def utility_to_label(utility):
-    utility = float(utility)
-    if utility <= 0.2:
-        return 'very low'
-    elif utility <= 0.4:
-        return 'low'
-    elif utility <= 0.6:
-        return 'average'
-    elif utility <= 0.8:
-        return 'good'
-    else:
-        return 'great'
+# # Preprocess data
+# def utility_to_label(utility):
+#     utility = float(utility)
+#     if utility <= 0.2:
+#         return 'very low'
+#     elif utility <= 0.4:
+#         return 'low'
+#     elif utility <= 0.6:
+#         return 'average'
+#     elif utility <= 0.8:
+#         return 'good'
+#     else:
+#         return 'great'
 
-df = pd.read_csv('data.txt', sep='\t', encoding='utf-16')
-df['utility'] = df['utility'].apply(utility_to_label)
-df.to_csv('data.txt', sep='\t', encoding='utf-16', index=False)
+# df = pd.read_csv('data.txt', sep='\t', encoding='utf-16')
+# df['utility'] = df['utility'].apply(utility_to_label)
+# df.to_csv('data.txt', sep='\t', encoding='utf-16', index=False)
 
