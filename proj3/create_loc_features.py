7334ba5057c43ecd0897f1b3a96fd9a173ca0a7d
richardwsong <richardwsong@gmail.com>
2024-03-24 15:04:50 -0500

    created feature vectors

--- proj3/create_loc_features.py
+++ proj3/create_loc_features.py
@@ -1,1 +1,24 @@
 # Create the feature vector for each location in the dataset 
+import pandas as pd 
+import numpy as np
+
+attractions_df = pd.read_csv('proj3/road_network_attractions.csv')
+themes_df = pd.read_csv('proj3/road_network_themes.csv')
+
+themes_list = themes_df.iloc[:, 0].tolist()
+themes_list = [theme.replace(" ", "") for theme in themes_list]
+feature_dict = dict()
+#print(themes_list)
+
+for index, row in attractions_df.iterrows():
+    location = row['Loc or Edge Label']
+    if location not in feature_dict:
+        feature_dict[location] = np.zeros(len(themes_list))
+
+    try:
+        themes = row['Themes'].split(',')
+        themes = [theme.strip() for theme in themes]
+        for theme in themes:
+            feature_dict[location][themes_list.index(theme)] += 1
+    except:
+        continue 
